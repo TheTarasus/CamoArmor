cc60d904fb38f37ecebaf36968429211a2ec29ec
TheTarasus <thetarasus.rus@ya.ru>
2023-06-10 07:00:05 +0400

    Update to 1.17.1

--- src/main/java/ru/thetarasus/camouflage/NetheriteDyeable.java
+++ src/main/java/ru/thetarasus/camouflage/NetheriteDyeable.java
@@ -4,7 +4,7 @@ import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.DyeableArmorItem;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtCompound;
 
 public class NetheriteDyeable extends CamouflageArmor {
 
@@ -19,7 +19,7 @@ public class NetheriteDyeable extends CamouflageArmor {
 
     @Override
     public int getColor(ItemStack stack) {
-        CompoundTag compoundTag = stack.getSubTag("display");
+        NbtCompound compoundTag = stack.getSubNbt("display");
         return compoundTag != null && compoundTag.contains("color", 99) ? compoundTag.getInt("color") : 0x756474;
     }
 }
